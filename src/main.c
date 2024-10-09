839feaf65eef0e9e9b7d70b27fa34069ffc66af2
Bruno <brunoomiguel10@hotmail.com>
2024-10-09 20:10:03 +0100

    Organizing functions

--- src/main.c
+++ src/main.c
@@ -3,28 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 10:35:10 by bruno             #+#    #+#             */
-/*   Updated: 2024/09/24 17:29:12 by bruno            ###   ########.fr       */
+/*   Updated: 2024/10/03 16:59:17 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void print_error(char *msg, int exit_code)
-{
-	ft_printf("%s", msg);
-	exit(exit_code);
-}
-
 int main(int ac, char **av)
 {
-	if(ac != 2)
-		print_error("invalid number of arguments", 0);
-	if(file_validation(av[1]))
-		print_error("invalid file name", 0);
-	map_validation(av[1]);
+	if (ac != 2)
+		return (printf("Invalid number of arguments\n"), 1);
+	if (!file_validation(av[1]))
+		return (1);
+	if (!map_validation(av[1]))
+		return (1);
+	
 	
 	return(0);
 }
